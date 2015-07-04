0d7dd3058bd62190e18d57feb89232cb2d686de9
Artem Lantsev <artln@artln-osx.local>
2015-07-04 23:40:59 +0300

    step 0

--- art/step0_repl.cpp
+++ art/step0_repl.cpp
@@ -3,16 +3,71 @@
 
 static const char* PROMPT = "user> ";
 
+//
+class repl_exception{};
+
+//
+std::string readline()
+{
+    std::string line;
+    if (!getline(std::cin, line))
+        throw repl_exception();
+    return line;
+};
+
+//
+std::string
+READ()
+{
+    return readline();
+}
+
+//
+std::string
+EVAL(const std::string& input)
+{
+    return input;
+}
+
+//
+std::string
+PRINT(const std::string& input)
+{
+    return input;
+}
+
+//
+std::string
+rep()
+{
+    auto&& line = READ();
+    auto&& ast = EVAL(line);
+    auto&& output = PRINT(ast);
+    return output;
+}
+
+//
+void repl()
+{
+    try
+    {
+        for (;;)
+        {
+            std::cout << PROMPT;
+            auto&& output = rep();
+            std::cout << output << std::endl;
+        }
+    }
+    catch (const repl_exception&)
+    {
+    }
+    std::cout << std::endl;
+}
+
+//
 int
 main(int, char**)
 {
-    std::string line;
-    std::cout << PROMPT;
-    while (getline(std::cin, line))
-    {
-        std::cout << line << std::endl;
-        std::cout << PROMPT;
-    }
-    std::cout << std::endl;
+    repl();
     return 0;
 }
