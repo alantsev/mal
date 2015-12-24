88202facfe0783313c91d2f713a52b52d061c719
art <alantsev@users.noreply.github.com>
2015-12-24 06:27:05 +0400

    step1 - parser - sork in progress

--- art/reader.cpp
+++ art/reader.cpp
@@ -3,57 +3,71 @@
 
 #include <string>
 #include <cctype>
+#include <cstdlib>
 
 ast 
 read_str (std::string line)
 {
-	ast_builder builder;
-/*
-	size_t last_delim_position = 0;
-	size_t current_position = 0;
+  ast_builder builder;
 
-	for (auto ch : line)
-	{
-		++current_position;
-		if (ch == '(')
-		{
-			last_delim_position = current_position;
-			builder.open_list ();
-		}
-		else if (ch == ')')
-		{
-			last_delim_position = current_position;
-			builder.close_list ();
-		}
-		else if (std::isspace (ch))
-		{
-			last_delim_position = current_position;
-		}
+  size_t last_delim_position = 0;
+  size_t current_position = 0;
 
-	}
-	// FIXME - 
-*/
-	builder.open_list ();
-	builder.add_symbol ("s1");
-	builder.add_symbol ("s2");
-	builder.add_int (5);
+  auto fnProcessToken = [&line, &builder] (size_t from, size_t to) 
+  {
+    if (from + 1 < to)
+    {
+      char *end;
+      int valInt = std::strtol(token.data () + from, &end, 10);
 
-	builder.open_list ();
-	builder.add_symbol ("s1");
-	builder.add_symbol ("s2");
-	builder.add_int (5);
-	builder.close_list ();
+      if (end == token.data () + to)
+      {
+        builder.add_int (valInt);
+      }
+      else
+      {
+        builder.add_symbol (line.substr (from, to - from));
+      }
+    }
+  };
 
-	builder.close_list ();
+  for (auto ch : line)
+  {
+    ++current_position;
+    bool isDelim = false;
+    if (ch == '(')
+    {
+      isDelim = true;
+      last_delim_position = current_position;
+      builder.open_list ();
+    }
+    else if (ch == ')')
+    {
+      isDelim = true;
+      last_delim_position = current_position;
+      builder.close_list ();
+    }
+    else if (std::isspace (ch))
+    {
+      isDelim = true;
+    }
 
-	return builder.build ();
+    if (isDelim)
+    {
+      fnProcessToken (last_delim_position, current_position);
+      last_delim_position = current_position;
+    }
+  }
+  fnProcessToken (last_delim_position, current_position);
+
+  return builder.build ();
 
 }
 
 std::string 
 pr_str (ast a_ast)
 {
-	if (!a_ast.m_root)
-		return {};
-	return a_ast.m_root->to_string ();
+  if (!a_ast.m_root)
+    return {};
+  return a_ast.m_root->to_string ();
 }
