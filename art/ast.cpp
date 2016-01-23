4571ee8389b1c9314f35eef983f7125af4dbfb41
Artem Lantsev <alantsev@users.noreply.github.com>
2016-01-23 01:04:10 +0000

    2 spaces indentation

--- art/ast.cpp
+++ art/ast.cpp
@@ -7,27 +7,27 @@
 std::string 
 ast_node_list::to_string () const // override
 {
-    std::string retVal = "(";
-    for (size_t i = 0, e = m_children.size (); i < e; ++i)
-    {
-        auto &&p = m_children [i];
-        if (i != 0)
-            retVal += " ";
-        retVal += p->to_string ();
-    }
-    retVal += ")";
+  std::string retVal = "(";
+  for (size_t i = 0, e = m_children.size (); i < e; ++i)
+  {
+    auto &&p = m_children [i];
+    if (i != 0)
+      retVal += " ";
+    retVal += p->to_string ();
+  }
+  retVal += ")";
 
-    return retVal;
+  return retVal;
 }
 
 ///////////////////////////////
 ast_node::ptr
 ast_node_list::clear_and_grab_first_child () 
 {
-    assert (size () > 0);
-    ast_node::ptr retVal = std::move (m_children [0]);
-    m_children.clear ();
-    return std::move (retVal);
+  assert (size () > 0);
+  ast_node::ptr retVal = std::move (m_children [0]);
+  m_children.clear ();
+  return std::move (retVal);
 }
 
 ///////////////////////////////
@@ -36,155 +36,155 @@ ast_node_list::clear_and_grab_first_child ()
 std::string 
 ast_node_vector::to_string () const // override
 {
-    std::string retVal = "[";
-    for (size_t i = 0, e = m_children.size (); i < e; ++i)
-    {
-        auto &&p = m_children [i];
-        if (i != 0)
-            retVal += " ";
-        retVal += p->to_string ();
-    }
-    retVal += "]";
+  std::string retVal = "[";
+  for (size_t i = 0, e = m_children.size (); i < e; ++i)
+  {
+    auto &&p = m_children [i];
+    if (i != 0)
+      retVal += " ";
+    retVal += p->to_string ();
+  }
+  retVal += "]";
 
-    return retVal;
+  return retVal;
 }
 
 ///////////////////////////////
 /// ast_node_atom_symbol class
 ///////////////////////////////
 ast_node_atom_symbol::ast_node_atom_symbol (std::string a_symbol)
-    : m_symbol (std::move (a_symbol))
+  : m_symbol (std::move (a_symbol))
 {}
 
 ///////////////////////////////
 std::string
 ast_node_atom_symbol::to_string () const // override
 {
-    // FIXME
-    return m_symbol;
+  // FIXME
+  return m_symbol;
 }
 
 ///////////////////////////////
 /// ast_node_atom_int class
 ///////////////////////////////
 ast_node_atom_int::ast_node_atom_int (int a_value)
-    : m_value (a_value)
+  : m_value (a_value)
 {}
 
 ///////////////////////////////
 std::string
 ast_node_atom_int::to_string () const // override
 {
-    // FIXME
-    return std::to_string (m_value);
+  // FIXME
+  return std::to_string (m_value);
 }
 
 ///////////////////////////////
 /// ast_node_callable_builtin class
 ///////////////////////////////
 ast_node_callable_builtin::ast_node_callable_builtin (std::string signature, ast_node_callable_builtin::builtin_fn fn)
-    : m_signature (std::move (signature))
-    , m_fn (fn)
+  : m_signature (std::move (signature))
+  , m_fn (fn)
 {}
 
 ///////////////////////////////
 ast_node::ptr 
 ast_node_callable_builtin::call (const call_arguments& args) const
 {
-    return m_fn (args);
+  return m_fn (args);
 }
 
 ///////////////////////////////
 /// ast_builder class
 ///////////////////////////////
 ast_builder::ast_builder ()
-    : m_meta_root (new ast_node_list {})
-    , m_current_stack ({m_meta_root.get ()})
+  : m_meta_root (new ast_node_list {})
+  , m_current_stack ({m_meta_root.get ()})
 {}
 
 ///////////////////////////////
 ast_builder&
 ast_builder::open_list ()
 {
-    std::unique_ptr<ast_node_list> child { new ast_node_list {} };
-    ast_node_list* child_ref = child.get ();
+  std::unique_ptr<ast_node_list> child { new ast_node_list {} };
+  ast_node_list* child_ref = child.get ();
 
-    m_current_stack.back ()->add_child (std::move (child));
-    m_current_stack.push_back (child_ref);
-    return *this;
+  m_current_stack.back ()->add_child (std::move (child));
+  m_current_stack.push_back (child_ref);
+  return *this;
 }
 
 ///////////////////////////////
 ast_builder& 
 ast_builder::close_list ()
 {
-    m_current_stack.back ()->as_or_throw<ast_node_list, mal_exception_parse_error> ();
-    m_current_stack.pop_back ();
-    if (m_current_stack.size () == 0) 
-        raise<mal_exception_parse_error> ();
+  m_current_stack.back ()->as_or_throw<ast_node_list, mal_exception_parse_error> ();
+  m_current_stack.pop_back ();
+  if (m_current_stack.size () == 0) 
+    raise<mal_exception_parse_error> ();
 
-    return *this;
+  return *this;
 }
 
 ///////////////////////////////
 ast_builder&
 ast_builder::open_vector ()
 {
-    std::unique_ptr<ast_node_vector> child { new ast_node_vector {} };
-    ast_node_vector* child_ref = child.get ();
+  std::unique_ptr<ast_node_vector> child { new ast_node_vector {} };
+  ast_node_vector* child_ref = child.get ();
 
-    m_current_stack.back ()->add_child (std::move (child));
-    m_current_stack.push_back (child_ref);
-    return *this;
+  m_current_stack.back ()->add_child (std::move (child));
+  m_current_stack.push_back (child_ref);
+  return *this;
 }
 
 ///////////////////////////////
 ast_builder& 
 ast_builder::close_vector ()
 {
-    m_current_stack.back ()->as_or_throw<ast_node_vector, mal_exception_parse_error> ();
-    m_current_stack.pop_back ();
-    if (m_current_stack.size () == 0) 
-        raise<mal_exception_parse_error> ();
+  m_current_stack.back ()->as_or_throw<ast_node_vector, mal_exception_parse_error> ();
+  m_current_stack.pop_back ();
+  if (m_current_stack.size () == 0) 
+    raise<mal_exception_parse_error> ();
 
-    return *this;
+  return *this;
 }
 
 ///////////////////////////////
 ast_builder& 
 ast_builder::add_symbol (std::string value)
 {
-    std::unique_ptr<ast_node_atom_symbol> child { new ast_node_atom_symbol {std::move (value)} };
-    m_current_stack.back ()->add_child (std::move (child));
-    return *this;
+  std::unique_ptr<ast_node_atom_symbol> child { new ast_node_atom_symbol {std::move (value)} };
+  m_current_stack.back ()->add_child (std::move (child));
+  return *this;
 }
 
 ///////////////////////////////
 ast_builder& 
 ast_builder::add_int (int value)
 {
-    std::unique_ptr<ast_node_atom_int> child { new ast_node_atom_int { value } };
-    m_current_stack.back ()->add_child (std::move (child));
-    return *this;
+  std::unique_ptr<ast_node_atom_int> child { new ast_node_atom_int { value } };
+  m_current_stack.back ()->add_child (std::move (child));
+  return *this;
 }
 
 ///////////////////////////////
 ast 
 ast_builder::build()
 {
-    if (m_current_stack.size () != 1) 
-        raise<mal_exception_parse_error> ();
+  if (m_current_stack.size () != 1) 
+    raise<mal_exception_parse_error> ();
 
-    const size_t level0_count = m_current_stack.back()->size (); 
-    if (level0_count > 1) 
-        raise<mal_exception_parse_error> ();
+  const size_t level0_count = m_current_stack.back()->size (); 
+  if (level0_count > 1) 
+    raise<mal_exception_parse_error> ();
 
-    if (level0_count == 0)
-        return ast {};
+  if (level0_count == 0)
+    return ast {};
 
-    ast retVal {std::move (m_current_stack.back()->as_or_throw<ast_node_list, mal_exception_parse_error> ()->clear_and_grab_first_child ())};
-    m_current_stack.clear ();
-    m_meta_root.reset ();
+  ast retVal {std::move (m_current_stack.back()->as_or_throw<ast_node_list, mal_exception_parse_error> ()->clear_and_grab_first_child ())};
+  m_current_stack.clear ();
+  m_meta_root.reset ();
 
-    return std::move (retVal);
+  return std::move (retVal);
 }
--- art/ast.h
+++ art/ast.h
@@ -12,14 +12,14 @@
 ///////////////////////////////
 enum class node_type_enum
 {
-    UNKNOWN = 0,
-    SYMBOL,
-    STRING,
-    INT,
-    LIST,
-    VECTOR,
-    CALLABLE,
-    NODE_TYPE_COUNT
+  UNKNOWN = 0,
+  SYMBOL,
+  STRING,
+  INT,
+  LIST,
+  VECTOR,
+  CALLABLE,
+  NODE_TYPE_COUNT
 };
 
 ///////////////////////////////
@@ -37,10 +37,10 @@ class ast_node_callable_builtin;
 class call_arguments
 {
 public:
-    virtual ~call_arguments () = default;
+  virtual ~call_arguments () = default;
 
-    virtual size_t size () const = 0;
-    virtual const ast_node& operator [] (size_t index) const = 0;
+  virtual size_t size () const = 0;
+  virtual const ast_node& operator [] (size_t index) const = 0;
 };
 
 
@@ -48,61 +48,61 @@ public:
 class ast_node
 {
 public:
-    using ptr = std::unique_ptr<ast_node>;
+  using ptr = std::unique_ptr<ast_node>;
 
-    ast_node () = default;
-    virtual ~ast_node () = default;
-    virtual std::string to_string () const = 0;
-    virtual node_type_enum type () const = 0;
-    virtual ast_node::ptr clone () const = 0;
+  ast_node () = default;
+  virtual ~ast_node () = default;
+  virtual std::string to_string () const = 0;
+  virtual node_type_enum type () const = 0;
+  virtual ast_node::ptr clone () const = 0;
 
-    //
-    template <typename T>
-    T* as ()
-    {
-        assert (type () == T::GET_TYPE ());
-        return static_cast<T*> (this);
-    }
-    template <typename T>
-    const T* as () const
-    {
-        assert (type () == T::GET_TYPE ());
-        return static_cast<const T*> (this);
-    }
+  //
+  template <typename T>
+  T* as ()
+  {
+    assert (type () == T::GET_TYPE ());
+    return static_cast<T*> (this);
+  }
+  template <typename T>
+  const T* as () const
+  {
+    assert (type () == T::GET_TYPE ());
+    return static_cast<const T*> (this);
+  }
 
-    template <typename T, typename TException>
-    T* as_or_throw ()
-    {
-        if (type () != T::GET_TYPE ())
-            raise<TException> (this->to_string ());
-        return static_cast<T*> (this);
-    }
-    template <typename T, typename TException>
-    const T* as_or_throw () const
-    {
-        if (type () != T::GET_TYPE ())
-            raise<TException> (this->to_string ());
-        return static_cast<const T*> (this);
-    }
+  template <typename T, typename TException>
+  T* as_or_throw ()
+  {
+    if (type () != T::GET_TYPE ())
+      raise<TException> (this->to_string ());
+    return static_cast<T*> (this);
+  }
+  template <typename T, typename TException>
+  const T* as_or_throw () const
+  {
+    if (type () != T::GET_TYPE ())
+      raise<TException> (this->to_string ());
+    return static_cast<const T*> (this);
+  }
 
-    template <typename T, typename TException>
-    T* as_or_zero ()
-    {
-        if (type () != T::GET_TYPE ())
-            return nullptr;
-        return static_cast<T*> (this);
-    }
-    template <typename T, typename TException>
-    const T* as_or_zero () const
-    {
-        if (type () != T::GET_TYPE ())
-            return nullptr;
-        return static_cast<const T*> (this);
-    }
+  template <typename T, typename TException>
+  T* as_or_zero ()
+  {
+    if (type () != T::GET_TYPE ())
+      return nullptr;
+    return static_cast<T*> (this);
+  }
+  template <typename T, typename TException>
+  const T* as_or_zero () const
+  {
+    if (type () != T::GET_TYPE ())
+      return nullptr;
+    return static_cast<const T*> (this);
+  }
 
 private:
-    ast_node (const ast_node&) = delete;
-    ast_node& operator = (const ast_node&) = delete;
+  ast_node (const ast_node&) = delete;
+  ast_node& operator = (const ast_node&) = delete;
 };
 
 ///////////////////////////////
@@ -110,15 +110,15 @@ template <node_type_enum NODE_TYPE>
 class ast_node_base : public ast_node
 {
 public:
-    node_type_enum type () const override
-    {
-        return NODE_TYPE;
-    }
+  node_type_enum type () const override
+  {
+    return NODE_TYPE;
+  }
 
-    static constexpr node_type_enum GET_TYPE ()
-    {
-        return NODE_TYPE;
-    }
+  static constexpr node_type_enum GET_TYPE ()
+  {
+    return NODE_TYPE;
+  }
 };
 
 ///////////////////////////////
@@ -130,75 +130,75 @@ class ast_node_atom : public ast_node_base<NODE_TYPE>
 class ast_node_atom_symbol : public ast_node_atom <node_type_enum::SYMBOL>
 {
 public:
-    ast_node_atom_symbol (std::string a_symbol);
-    std::string to_string () const override;
-    ast_node::ptr clone () const override
-    {
-        return ast_node::ptr (new ast_node_atom_symbol (m_symbol));
-    }
+  ast_node_atom_symbol (std::string a_symbol);
+  std::string to_string () const override;
+  ast_node::ptr clone () const override
+  {
+    return ast_node::ptr (new ast_node_atom_symbol (m_symbol));
+  }
 
-    const std::string& symbol () const
-    {
-        return m_symbol;
-    }
+  const std::string& symbol () const
+  {
+    return m_symbol;
+  }
 
 private:
-    std::string m_symbol;
+  std::string m_symbol;
 };
 
 ///////////////////////////////
 class ast_node_atom_int : public ast_node_atom <node_type_enum::INT>
 {
 public:
-    ast_node_atom_int (int a_value);
+  ast_node_atom_int (int a_value);
 
-    std::string to_string () const override;
-    ast_node::ptr clone () const override
-    {
-        return ast_node::ptr (new ast_node_atom_int (m_value));
-    }
+  std::string to_string () const override;
+  ast_node::ptr clone () const override
+  {
+    return ast_node::ptr (new ast_node_atom_int (m_value));
+  }
 
-    int value () const
-    {
-        return m_value;
-    }
+  int value () const
+  {
+    return m_value;
+  }
 
 private:
-    int m_value;
+  int m_value;
 };
 
 ///////////////////////////////
 class ast_node_container_base : public ast_node
 {
 public:
-    size_t size () const
-    {
-        return m_children.size ();
-    }
+  size_t size () const
+  {
+    return m_children.size ();
+  }
 
-    void add_child (ast_node::ptr child)
-    {
-        m_children.push_back (std::move (child));
-    }
+  void add_child (ast_node::ptr child)
+  {
+    m_children.push_back (std::move (child));
+  }
 
-    const ast_node* operator [] (size_t index) const
-    {
-        assert (index < size ());
-        return m_children[index].get ();
-    }
+  const ast_node* operator [] (size_t index) const
+  {
+    assert (index < size ());
+    return m_children[index].get ();
+  }
 
-    template <typename Fn>
-    void map (const Fn& fn)
+  template <typename Fn>
+  void map (const Fn& fn)
+  {
+    for (auto && v : m_children)
     {
-        for (auto && v : m_children)
-        {
-            auto newV = fn (std::move (v));
-            v = std::move (newV);
-        }
+      auto newV = fn (std::move (v));
+      v = std::move (newV);
     }
+  }
 
 protected:
-    std::vector<ast_node::ptr> m_children;
+  std::vector<ast_node::ptr> m_children;
 };
 
 ///////////////////////////////
@@ -206,123 +206,123 @@ template <node_type_enum NODE_TYPE, typename derived>
 class ast_node_container_crtp : public ast_node_container_base
 {
 public:
-    node_type_enum type () const override
-    {
-        return NODE_TYPE;
-    }
+  node_type_enum type () const override
+  {
+    return NODE_TYPE;
+  }
 
-    static constexpr node_type_enum GET_TYPE ()
-    {
-        return NODE_TYPE;
-    }
+  static constexpr node_type_enum GET_TYPE ()
+  {
+    return NODE_TYPE;
+  }
 
-    ast_node::ptr clone () const override
+  ast_node::ptr clone () const override
+  {
+    auto new_list = std::make_unique<derived> ();
+    for (auto &&v : m_children)
     {
-        auto new_list = std::make_unique<derived> ();
-        for (auto &&v : m_children)
-        {
-            new_list->m_children.push_back (v->clone ());
-        }
-        return ast_node::ptr (new_list.release ());
+      new_list->m_children.push_back (v->clone ());
     }
+    return ast_node::ptr (new_list.release ());
+  }
 };
 
 ///////////////////////////////
 class ast_node_list : public ast_node_container_crtp <node_type_enum::LIST, ast_node_list>
 {
 public:
-    std::string to_string () const override;
+  std::string to_string () const override;
 
-    // FIXME - do we need it?
-    ast_node::ptr clear_and_grab_first_child ();
+  // FIXME - do we need it?
+  ast_node::ptr clear_and_grab_first_child ();
 
-    ///////////////////////////////
-    class list_call_arguments : public call_arguments
+  ///////////////////////////////
+  class list_call_arguments : public call_arguments
+  {
+  public:
+    list_call_arguments (const ast_node_list* owner, size_t offset, size_t count)
+      : m_owner (owner)
+      , m_offset (offset)
+      , m_count (count)
+    {}
+    size_t size () const override
     {
-    public:
-        list_call_arguments (const ast_node_list* owner, size_t offset, size_t count)
-            : m_owner (owner)
-            , m_offset (offset)
-            , m_count (count)
-        {}
-        size_t size () const override
-        {
-            return m_count;
-        }
+      return m_count;
+    }
 
-        const ast_node& operator [] (size_t index) const override
-        {
-            return *(*m_owner) [index + m_offset];
-        }
+    const ast_node& operator [] (size_t index) const override
+    {
+      return *(*m_owner) [index + m_offset];
+    }
 
-    private:
-        const ast_node_list* m_owner;
-        size_t m_offset;
-        size_t m_count;
-    };
+  private:
+    const ast_node_list* m_owner;
+    size_t m_offset;
+    size_t m_count;
+  };
 };
 
 ///////////////////////////////
 class ast_node_vector : public ast_node_container_crtp <node_type_enum::VECTOR, ast_node_vector>
 {
 public:
-    std::string to_string () const override;
+  std::string to_string () const override;
 };
 
 ///////////////////////////////
 class ast_node_callable : public ast_node_base  <node_type_enum::CALLABLE>
 {
 public:
-    virtual ast_node::ptr call (const call_arguments&) const = 0;
+  virtual ast_node::ptr call (const call_arguments&) const = 0;
 };
 
 ///////////////////////////////
 class ast_node_callable_builtin : public ast_node_callable
 {
 public:
-    using builtin_fn = ast_node::ptr (*) (const call_arguments&);
-    ast_node_callable_builtin (std::string signature, builtin_fn fn);
+  using builtin_fn = ast_node::ptr (*) (const call_arguments&);
+  ast_node_callable_builtin (std::string signature, builtin_fn fn);
 
-    std::string to_string () const override
-    {
-        return m_signature;
-    }
-    ast_node::ptr clone () const override
-    {
-        return ast_node::ptr (new ast_node_callable_builtin (m_signature, m_fn));
-    }
+  std::string to_string () const override
+  {
+    return m_signature;
+  }
+  ast_node::ptr clone () const override
+  {
+    return ast_node::ptr (new ast_node_callable_builtin (m_signature, m_fn));
+  }
 
-    ast_node::ptr call (const call_arguments& args) const override;
+  ast_node::ptr call (const call_arguments& args) const override;
 
 private:
-    std::string m_signature;
-    builtin_fn m_fn;
+  std::string m_signature;
+  builtin_fn m_fn;
 };
 
 ///////////////////////////////
 struct ast
 {
-    ast_node::ptr m_root;
+  ast_node::ptr m_root;
 };
 
 ///////////////////////////////
 class ast_builder
 {
 public:
-    ast_builder ();
+  ast_builder ();
 
-    ast_builder& open_list ();
-    ast_builder& close_list ();
+  ast_builder& open_list ();
+  ast_builder& close_list ();
 
-    ast_builder& open_vector ();
-    ast_builder& close_vector ();
+  ast_builder& open_vector ();
+  ast_builder& close_vector ();
 
-    ast_builder& add_symbol (std::string);
-    ast_builder& add_int (int);
+  ast_builder& add_symbol (std::string);
+  ast_builder& add_int (int);
 
-    ast build();
+  ast build();
 
 private:
-    std::unique_ptr<ast_node_list> m_meta_root;
-    std::vector<ast_node_container_base*> m_current_stack;
+  std::unique_ptr<ast_node_list> m_meta_root;
+  std::vector<ast_node_container_base*> m_current_stack;
 };
